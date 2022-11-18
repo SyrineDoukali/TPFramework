7812700b32bae6c2cd37d01002c67e4aacf1c5f6
azizche <azizcherif14@gmail.com>
2022-11-18 17:52:33 +0100

    tp22

--- src/todo-module/todo/todoservice/todoservice.service.ts
+++ src/todo-module/todo/todoservice/todoservice.service.ts
@@ -1,6 +1,6 @@
 import { Body, Delete, Get, Injectable, NotFoundException, Param, Post } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Like, QueryBuilder, Repository } from 'typeorm';
 import { AjouterDto } from '../../dtos/ajouter-dto';
 import { MajTodo } from '../../dtos/maj-todo';
 import { TodoEntity, TodoStatusEnum } from '../../entities/todo';
@@ -14,29 +14,29 @@ export class TodoserviceService {
   ) {
     
   }
-    private readonly todoList = [];
-    private findById(id: string): TodoEntity{
+  private readonly todoList = [];
+  private findById(id: string): TodoEntity {
           
-        const todo = this.todoList.find((newTodo)=> newTodo.id == newTodo);
-       if (!todo){
-        throw new NotFoundException(' Todo not found')
-       }
-       return todo
+    const todo = this.todoList.find((newTodo) => newTodo.id == newTodo);
+    if (!todo) {
+      throw new NotFoundException(' Todo not found')
     }
+    return todo
+  }
     
-  async   getTodos(){
-        return this.todoList;
-    }
+  async getTodos() {
+    return this.todoList;
+  }
 
     
- async   getTodoById(@Param('id') id : string): Promise<TodoEntity>{
-       return this.findById(id)
-    }
+  async getTodoById(@Param('id') id: string): Promise<TodoEntity> {
+    return this.findById(id)
+  }
 
   async removeTodoavecTypeOrm(id: number) {
     const todoToRemove = await this.todoRepository.findOne({ where: { id } });
     if (!todoToRemove) {
-      throw   new NotFoundException('Todo not found');
+      throw new NotFoundException('Todo not found');
     }
     return await this.todoRepository.remove(todoToRemove);
     
@@ -52,66 +52,108 @@ export class TodoserviceService {
     const newtodo = await this.todoRepository.preload({
       id,
       ...todo
-    }) 
+    })
     return await this.todoRepository.save(todo);
     
   }
-   async addTodo(@Body() addtodo: AjouterDto){
-       const date = new Date();
-       const id = uuidv4();
-       const {name, description} = addtodo;
-       const newTodo = {
-          id: id,
-          name : name,
-          description: description,
-          createdAt: date,
-          status : TodoStatusEnum.waiting
-       }
-       this.todoList.push(newTodo);
-
+  async addTodo(@Body() addtodo: AjouterDto) {
+    const date = new Date();
+    const id = uuidv4();
+    const { name, description } = addtodo;
+    const newTodo = {
+      id: id,
+      name: name,
+      description: description,
+      createdAt: date,
+      status: TodoStatusEnum.waiting
     }
+    this.todoList.push(newTodo);
+
+  }
 
     
-  async  updateTodo(@Param('id') id, @Body() updatetodo: MajTodo){
-        const todo = await this.getTodoById(id);
-        // if (todo.name) {
-        //     todo.name = updatetodo.name;
-        // }
+  async updateTodo(@Param('id') id, @Body() updatetodo: MajTodo) {
+    const todo = await this.getTodoById(id);
+    // if (todo.name) {
+    //     todo.name = updatetodo.name;
+    // }
        
-        if (todo.description) {
-            todo.description = updatetodo.description;
-        }
+    if (todo.description) {
+      todo.description = updatetodo.description;
     }
+  }
 
     
-  async  deleteTodo( @Param('id') id){
-        const index = this.todoList.findIndex((todo) => todo.id === id);
-        if (index >= 0 ) {
-            this.todoList.splice(index, 1);
-          } else {
-            throw new NotFoundException('Todo not found');
-          }
+  async deleteTodo(@Param('id') id) {
+    const index = this.todoList.findIndex((todo) => todo.id === id);
+    if (index >= 0) {
+      this.todoList.splice(index, 1);
+    } else {
+      throw new NotFoundException('Todo not found');
+    }
   }
   async softRemovetodo(id: number) {
-      const todoToRemove = await this.todoRepository.findOne({ where: { id } });
+    const todoToRemove = await this.todoRepository.findOne({ where: { id } });
     if (!todoToRemove) {
-      throw   new NotFoundException('Todo not found');
+      throw new NotFoundException('Todo not found');
     }
     return await this.todoRepository.softRemove(todoToRemove)
   }
   async rcovertodo(id: number) {
-     const todoToRemove = await this.todoRepository.findOne({ where: { id } });
+    const todoToRemove = await this.todoRepository.findOne({ where: { id } });
     if (!todoToRemove) {
-      throw   new NotFoundException('Todo not found');
+      throw new NotFoundException('Todo not found');
     }
-    this.todoRepository.recover( todoToRemove)
+    return await this.todoRepository.recover(todoToRemove)
   }
 
+  async stattodoNumberbyStatus() {
+    const query = this.todoRepository.createQueryBuilder("todo");
+    return await query.select("cv.status,count(cv.id)").groupBy("cv.status").getRawMany();
+  }
+  async getalltodos() {
     
+    return await this.todoRepository.find()
+  }
+  async querytodobydescriptionornameorstatus(ch: String, st: TodoStatusEnum) {
+    return await this.todoRepository.find({
+      where: [
+        {
+          status: st,
+        },
+        {
+          description: Like(`%${ch}%`),
+          status: st,
+        }
+      ],
+    });
+  }
+  
+  
+  async querytodobydescriptionornameorstatusv2(ch: String, st: TodoStatusEnum) {
+    return await this.todoRepository.find({
+      where: [
+        {
+          status: st,
+        },
+        {
+          description: Like(`%${ch}%`),
+        },
+        {
+          status: st,
+        }
+      
+      ],
+    });
 
+  }
+  async getTodoPagination(param): Promise<TodoEntity[]> {
+    return await this.todoRepository.find({
+      skip: (param.page - 1) * param.take,
+      take: param.take,
+    });
+  }
 }
-
-
 function uuidv4() {
     throw new Error('Function not implemented.');
 }
